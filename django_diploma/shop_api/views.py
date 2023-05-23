5c7e9ef9cd2c3131499e55bfc7100db9a92d2119
Sergey Strakhov <x-ser@yandex.ru>
2023-05-23 23:22:34 +0300

    Sign in, sign out, sign up added

--- django_diploma/shop_api/views.py
+++ django_diploma/shop_api/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+import json
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth.hashers import make_password
+from .models import Profile
 
-# Create your views here.
+
+def get_data(request: Request) -> dict:
+    print(request.data)
+    return json.loads(next(iter(request.data)))
+
+
+class SignIn(APIView):
+    def post(self, request: Request) -> Response:
+        auth_data = get_data(request)
+        username = auth_data.get('username')
+        password = auth_data.get('password')
+        user = authenticate(
+            self.request,
+            username=username,
+            password=password,)
+        if user:
+            login(request=self.request, user=user)
+            return Response(status=200)
+        else:
+            return Response(status=401)
+
+
+class SignUp(APIView):
+    def post(self, request: Request) -> Response:
+        register_data = get_data(request)
+        username = register_data.get('username')
+        password = make_password(register_data.get('password'))
+        print(register_data)
+        try:
+            user = User.objects.get(username=username)
+            if user.password != password:
+                return Response(status=401)
+        except User.DoesNotExist:
+            user = User.objects.create(
+                username=username,
+                password=password,
+            )
+            Profile.objects.create(user=user)
+
+
+        login(request=self.request, user=user)
+
+        return Response(status=200)
